4e67d9be7a407d48779e9f5993026a0264bc0b76
RhenryBubb <t1234561916@gmail.com>
2019-08-16 17:46:06 -0300

    updts

--- scroll.js
+++ scroll.js
@@ -6,14 +6,26 @@ $ ('#topo').click(function scrollAutomatico(e){
         scrollTop: targetOffset 
     	}, 1000);
 });
+
+//Scroll automatico 2.
+
 $('.voltaTopo').click(function scrollAutomatico2(e){
 	e.preventDefault();
 	$('html, body').animate({scrollTop:0}, 1000)
 });
+
+//Função para abrir a lista de afazeres e feitos.
+
 $('.verTabela').click(function verTarefas(e){
 	e.preventDefault();
 	var element = document.createElement('li');
-	var pega;
-	element.innerHTML += pega = JSON.parse(localStorage.getItem('taskList'));
+	var pega1 = window.localStorage.getItem('taskList');
+	var pega2 = window.localStorage.getItem('taskState');
+	var pega3 = window.localStorage.getItem('taskHora');
+	var pega4 = window.localStorage.getItem('taskFeita');
+	element.innerHTML += pega1 +' | ';
+	element.innerHTML += pega2 +' | ';
+	element.innerHTML += pega3 +' | ';
+	element.innerHTML += pega4;
 	listWrapper.appendChild(element);
 });
