0a46552afc18d49caffa4b5437c2981dd85134d6
RhenryBubb <t1234561916@gmail.com>
2019-08-20 17:50:56 -0300

    adição de imagens, correção de bugs, e a modificações em alguns aspectos

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -16,11 +16,6 @@ $('.voltaTopo').click(function scrollAutomatico2(e){
 
 //Função para abrir a lista de afazeres e feitos.
 
-$('.verTabela').click(function verTarefas(e){
-	e.preventDefault();
-	var element = document.createElement('li');
-	var pega = JSON.parse(window.localStorage.getItem('taskList'));
-	 element.innerHTML += pega;
-	listWrapper.appendChild(element);
-	console.log(pega);
+ $('.verTabela').click(function verTarefas(e){
+  e.preventDefault();
 });
